656aa984d9d658526bbc77188ffb9293e71423ff
Brandon Mitschke <bmits002@bluebonnetelectric.coop>
2020-01-06 15:57:45 -0600

    1-6-20 Commit
    Added in a dropdown button but its not fully functional yet, also added in another button, cleaned it up a little and started using the CSS and JS files accordingly.

--- Sandbox/hello-world/js-lib/main.js
+++ Sandbox/hello-world/js-lib/main.js
@@ -1,7 +1,17 @@
-<!DOCTYPE html>
-<html>
-<body>
-<button 
-     
-</body>
-</html>
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
